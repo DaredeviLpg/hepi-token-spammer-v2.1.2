8f3d3ffd913654c89735ca398b217e1f1bdb5bde
DaredeviL MenZ <82829983+DaredeviLpg@users.noreply.github.com>
2021-09-28 12:17:01 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+spam_msg="""
+spam message
+"""
+multithred_daemon = True #true or false, this dosent affect the speed
+_status = "status text"
+
+embed=dict(
+title="embed title",
+description="embed description", 
+color=697364,
+thumbnail=dict(url="https://someimageurl.png"),
+image=dict(url="https://someimageurl.png"),
+footer=dict(text="embed footer")
+)
+embed_spam =False
